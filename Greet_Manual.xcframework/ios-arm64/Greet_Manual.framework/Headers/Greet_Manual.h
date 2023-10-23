3fa3a7ed0bd15f3e7544b03aa3ab9f7934a7aa77
Aanal Trivedi <aanal.trivedi@innodeed.com>
2023-10-23 11:20:51 +0530

    framework added

---
+++ Greet_Manual.xcframework/ios-arm64/Greet_Manual.framework/Headers/Greet_Manual.h
@@ -0,0 +1,18 @@
+//
+//  Greet_Manual.h
+//  Greet_Manual
+//
+//  Created by SAPPHIRE on 20/10/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Greet_Manual.
+FOUNDATION_EXPORT double Greet_ManualVersionNumber;
+
+//! Project version string for Greet_Manual.
+FOUNDATION_EXPORT const unsigned char Greet_ManualVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Greet_Manual/PublicHeader.h>
+
+
